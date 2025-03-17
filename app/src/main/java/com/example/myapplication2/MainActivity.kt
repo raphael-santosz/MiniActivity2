30f4d5161b45c388280f6d90a1349c463a70592b
Raphael Augusto Santos <asantos.raphael@gmail.com>
2025-03-17 09:39:02 +0100

    padding from step 4 added.

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication2.ui.theme.MyApplication2Theme
@@ -38,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary) {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier.padding(24.dp)
         )
     }
 }
